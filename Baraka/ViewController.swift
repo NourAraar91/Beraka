c71be1a4249d8c3cee0ae76719c6be3cfe078b22
nour_araar <nour_araar_2009@yahoo.com>
2022-07-16 21:51:30 +0800

    Initial Commit

---
+++ Baraka/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Baraka
+//
+//  Created by Nour Araar on 7/16/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
